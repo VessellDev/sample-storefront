a5b3fe6d5a340c8c8154aa0bf1305e9ba1e25c68
dezainer <gameboymasterz@gmail.com>
2022-07-23 07:58:05 -0300

    products integration

--- pages/[produtos]/index.tsx
+++ pages/[produtos]/index.tsx
@@ -1,120 +1,118 @@
-import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next'
-import styles from './produtos.module.css'
-import { mockCategories, mockProducts } from 'mock'
-import Product from 'components/product'
-import Filters from 'components/filters'
-import Logo from 'components/logo'
-import Cart from 'components/cart/cart'
-import Breadcrumbs from 'components/breadcrumbs'
-import { CategoryType } from 'types/category'
-import classNames from 'classnames'
-import { useEffect, useState } from 'react'
-import { useLocomotiveScroll } from 'react-locomotive-scroll'
-import VessellSDK from 'sdk'
-
-export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  try {
-    const products = await VessellSDK.productSearch([{
-      
-    }, {
-      activeFilter: {
-        attributeOptions: true,
-        categories: true
-      },
-      filterableAttributes: {
-        attributeId: true,
-        attributeName: true,
-        attributeType: true,
-        options: {
-          quantity: true,
-          attributeOption: {
-            "...on ProductAttributeOptionColor": {
-              color: true,
-              name: true
-            },
-            "...on ProductAttributeOptionImage": {
-              name: true
-            },
-            "...on ProductAttributeOptionText": {
-              name: true
-            }
-          } 
-        }
-      },
-      filterableCategories: {
-        category: {
-          id: true,
-          name: true
-        },
-        quantity: true
-      },
-      items: {
-        edges: {
-          node: {
-            name: true,
-            price: {
-              minPrice: true
-            }
-          }
-        }
-      }
-    }])
-  
-    console.dir(products, { depth: null })
-  
-    const category = mockCategories.find(category => (
-      category.slug === ctx.query.produtos
-    ))
-  
-    if (!category) return { notFound: true }
-  
-    return {
-      props: { category }
-    }
-  } catch (err) {
-    console.dir(err, { depth: null })
-  }
-}
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next"
+import styles from "./produtos.module.css"
+import Product from "components/product"
+import Filters from "components/filters"
+import Logo from "components/logo"
+import Cart from "components/cart/cart"
+import Breadcrumbs from "components/breadcrumbs"
+import classNames from "classnames"
+import { useEffect, useState } from "react"
+import { useLocomotiveScroll } from "react-locomotive-scroll"
+import SDK from "sdk"
+import { GraphQLTypes } from "@vessell/sdk/lib/zeus"
 
 interface ProductsProps {
-  category: CategoryType
+  products: GraphQLTypes["ProductSearchResult"]["items"]["nodes"]
+  category: GraphQLTypes["ProductCategory"]
 }
 
-const Products: NextPage<ProductsProps> = ({ category }) => {
+const Products: NextPage<ProductsProps> = ({ products, category }) => {
   const { scroll } = useLocomotiveScroll()
   const [scrollY, setScrollY] = useState(0)
   const scrolling = scrollY > 0
 
   useEffect(() => {
-    scroll?.on('scroll', (args: any) => {
+    scroll?.on("scroll", (args: any) => {
       setScrollY(args.delta.y)
     })
   }, [scroll])
 
   return (
-    <div data-scroll-section id='container'>
+    <div data-scroll-section id="container">
       <header
-        data-scroll data-scroll-sticky data-scroll-target='#container'
+        data-scroll
+        data-scroll-sticky
+        data-scroll-target="#container"
         className={styles.header}
       >
-        <div className={classNames(styles.container, { [styles.scrolling]: scrolling })}>
+        <div
+          className={classNames(styles.container, {
+            [styles.scrolling]: scrolling,
+          })}
+        >
           <div className={styles.left}>
             <Logo />
-            <Breadcrumbs crumbs={[{ label: category.label, href: category.slug }]} />
+            <Breadcrumbs
+              crumbs={[{ label: category.name, href: category.slug }]}
+            />
           </div>
           <div className={styles.right}>
-            <Filters />
+            {/* <Filters /> */}
             <Cart />
           </div>
         </div>
       </header>
       <section className={styles.grid}>
-        {mockProducts.map(product => (
+        {products.map((product) => (
           <Product key={product.id} {...product} />
         ))}
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const slug = ctx.query.produtos as string
+
+  const products = await SDK.productSearch([
+    {
+      filter: {
+        categorySlug: slug,
+      },
+    },
+    {
+      items: {
+        nodes: {
+          id: true,
+          name: true,
+          slug: true,
+          mainImage: {
+            asset: {
+              url: true,
+            },
+          },
+          price: {
+            minPrice: true,
+          },
+        },
+      },
+    },
+  ])
+
+  const categories = await SDK.productCategories([
+    {
+      filter: {
+        slug: {
+          eq: slug,
+        },
+      },
+    },
+    {
+      nodes: {
+        name: true,
+        slug: true,
+      },
+    },
+  ])
+
+  return {
+    props: {
+      products: products.items.nodes,
+      category: categories.nodes[0],
+    },
+  }
 }
 
 export default Products
