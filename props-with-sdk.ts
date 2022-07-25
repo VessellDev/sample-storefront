d355818de5c36fcf6efcabcdb92857818d1758de
Wyllyan Sapieginski <wyllyan@wyllyan.com>
2022-07-25 18:10:23 -0300

    Implement a wrapper to the getServerSideProps function to setup the SDK with a project code.

---
+++ props-with-sdk.ts
@@ -0,0 +1,17 @@
+import { SDK as SDKType } from "@vessell/sdk/lib/types/sdk"
+import { GetServerSideProps } from "next"
+import SDK from "sdk"
+
+export const getServerSidePropsWithSDK = <P>(handler: (SDK: SDKType) => GetServerSideProps<P>): GetServerSideProps<P> => {
+  return async (context) => {
+    const { projectCode } = context.query
+
+    if (!projectCode) {
+      return { notFound: true }
+    }
+
+    SDK.setProjectCode(projectCode as string)
+
+    return handler(SDK)(context)
+  }
+}
