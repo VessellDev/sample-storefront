15677e2951b734f6d63cd0f052822a2336cfb74b
dezainer <gameboymasterz@gmail.com>
2023-01-22 14:39:54 -0300

    Personal info masks and validation

--- components/checkout/stepForms/personalInfoForm.tsx
+++ components/checkout/stepForms/personalInfoForm.tsx
@@ -2,6 +2,7 @@ import { LoadingButton } from '@mui/lab'
 import { Box, TextField } from '@mui/material'
 import { FC, useEffect } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import InputMask from 'react-input-mask'
 import { useMutation } from 'react-query'
 import SDK from 'sdk'
 import { StepFormProps } from '../step'
@@ -22,6 +23,7 @@ const PersonalInfoForm: FC<StepFormProps> = ({
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm<PersonalInfoFormInputs>({ defaultValues: purchase.customer })
 
   const { mutate, isLoading } = useMutation(
@@ -33,7 +35,11 @@ const PersonalInfoForm: FC<StepFormProps> = ({
       return SDK.request('mutation')({
         updateCustomer: [
           {
-            input: { name, phoneNumber, identificationNumber },
+            input: {
+              name,
+              phoneNumber: phoneNumber.replace(/\D/g, ''),
+              identificationNumber: identificationNumber.replace(/\D/g, ''),
+            },
           },
           { id: true },
         ],
@@ -60,20 +66,39 @@ const PersonalInfoForm: FC<StepFormProps> = ({
         error={Boolean(errors.name)}
       />
       <Box display="flex" alignItems="center" gap={1}>
-        <TextField
-          label="CPF"
-          type="number"
-          fullWidth
+        <InputMask
+          mask="999.999.999-99"
           {...register('identificationNumber', { required: true })}
-          error={Boolean(errors.identificationNumber)}
-        />
-        <TextField
-          label="Celular"
-          type="number"
-          fullWidth
+          onChange={(e) => setValue('identificationNumber', e.target.value)}
+        >
+          {() => (
+            <TextField
+              label="CPF"
+              fullWidth
+              error={Boolean(errors.identificationNumber)}
+              helperText={
+                errors.identificationNumber && 'Insira seu CPF para continuar'
+              }
+            />
+          )}
+        </InputMask>
+        <InputMask
+          mask="(99)99999-9999"
           {...register('phoneNumber', { required: true })}
-          error={Boolean(errors.phoneNumber)}
-        />
+          onChange={(e) => setValue('phoneNumber', e.target.value)}
+        >
+          {() => (
+            <TextField
+              label="Celular"
+              fullWidth
+              error={Boolean(errors.phoneNumber)}
+              helperText={
+                errors.identificationNumber &&
+                'Insira seu Telefone para continuar'
+              }
+            />
+          )}
+        </InputMask>
       </Box>
       <Box display="flex" justifyContent="flex-end">
         <LoadingButton variant="text" type="submit" disabled={isLoading}>
